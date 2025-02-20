989106414a7705ceba7cff09ab4b886aa8a5f251
Arun <arunbana612@gmail.com>
2025-02-21 01:44:47 +0530

    db link changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/food_del')
+    await mongoose.connect('mongodb+srv://arunpratapkhichi62:WoolZMn07uUqpmxO@cluster0.vrjub.mongodb.net/')
         .then(() => console.log("DB connected successfully"))
         .catch((error) => console.log("Error: DB not connected", error));
 };
