6346ce6e92a8e61c43630b944cffea4b48361c3d
Arun <arunbana612@gmail.com>
2025-02-21 00:49:44 +0530

    changes7

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -12,7 +12,7 @@ const [token,setToken] = useState("");
 const [food_list,setFoodlist] = useState([]);
 
 //const url = "http://localhost:1200"
-const url = "https://food-del-blond.vercel.app";
+const url = "https://food-del-two-delta.vercel.app";
 console.log("url ha ",url)
 
 const addToCart = async (itemId) => {
