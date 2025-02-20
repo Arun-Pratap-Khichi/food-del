4df2790059c9a307a6304076074ce22e4b01aa4f
Arun <arunbana612@gmail.com>
2025-02-21 00:04:31 +0530

    changes4

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -12,7 +12,7 @@ const [token,setToken] = useState("");
 const [food_list,setFoodlist] = useState([]);
 
 //const url = "http://localhost:1200"
-const url = import.meta.env.VITE_BACKEND_URL;
+const url = "https://food-del-blond.vercel.app";
 console.log("url ha ",url)
 
 const addToCart = async (itemId) => {
