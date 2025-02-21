308a56bf07f3da4e4cf0ce04257db0d213da886c
Arun <arunbana612@gmail.com>
2025-02-21 11:39:08 +0530

    changes 8

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ connectDB();
 
 
  // api endpoints
- app.use("/api/food",foodRouter)
+ //app.use("/api/food",foodRouter)
  app.use("/images",express.static('uploads'))
  app.use("/api/user",userRouter)
  app.use("/api/cart",cartRouter)
